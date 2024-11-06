7a509564f2c46c895fab8e8d6300384935d82fb2
cnux9 <65856226+cnux9@users.noreply.github.com>
2024-11-07 00:13:51 +0900

    Time: 9 ms (14.78%), Space: 45 MB (6.14%) - LeetHub

--- 0065-valid-number/0065-valid-number.java
+++ 0065-valid-number/0065-valid-number.java
@@ -1,6 +1,6 @@
 import java.util.regex.Pattern;
 class Solution {
-    private static final Pattern EXPONENT_PATTERN = Pattern.compile(".+[eE]{1}.+");
+    private static final Pattern EXPONENT_PATTERN = Pattern.compile("^[^eE]*[eE]{1}[^eE]*$");
     private static final Pattern INTEGER_PATTERN = Pattern.compile("^[+-]?[\\d]+$");
     private static final Pattern DECIMAL_PATTERN = Pattern.compile("^[+-]?[\\d]*[.]?[\\d]*");
     private static final Pattern DIGITS_PATTERN = Pattern.compile("[\\d]+");
@@ -11,31 +11,21 @@ class Solution {
                 return false;
             } else if (strings.length==2) {
                 return (isInteger(strings[0]) || isDecimal(strings[0])) && isInteger(strings[1]);
-            } else {
-                System.out.println("somethins's wrong");
             }
         }
         return isInteger(s) || isDecimal(s);
     }
 
     private boolean isInteger(String s) {
-        System.out.println("integer");
+        // System.out.println("integer");
         return INTEGER_PATTERN.matcher(s).matches();
     }
 
     private boolean isDecimal(String s) {
-        System.out.println("decimal");
+        // System.out.println("decimal");
         if (!DIGITS_PATTERN.matcher(s).find()) {
-            System.out.println("dd");
             return false;
         }
         return DECIMAL_PATTERN.matcher(s).matches();
-        // String[] strings = s.split(".");
-        // if (strings.length>2) {
-        //     return false;
-        // } else if (strings.length==2) {
-
-        // }
-        // return isInteger(strings[0]);
     }
 }
