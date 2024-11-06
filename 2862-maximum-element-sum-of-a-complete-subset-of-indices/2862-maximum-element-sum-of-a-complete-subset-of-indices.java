52fddb69af33250ce7614b112e3eb693aa9682ce
cnux9 <65856226+cnux9@users.noreply.github.com>
2024-11-06 11:45:11 +0900

    Time: 93 ms (20%), Space: 46 MB (28%) - LeetHub

--- 2862-maximum-element-sum-of-a-complete-subset-of-indices/2862-maximum-element-sum-of-a-complete-subset-of-indices.java
+++ 2862-maximum-element-sum-of-a-complete-subset-of-indices/2862-maximum-element-sum-of-a-complete-subset-of-indices.java
@@ -2,15 +2,13 @@ import java.util.stream.IntStream;
 class Solution {
     public long maximumSum(List<Integer> nums) {
         long maxSum = 0;
-
         for (int i=1;i<=nums.size();i++) {
-            
             int[] subset = getSubset(i,nums.size());
             long sum = IntStream.of(subset).mapToLong(j -> nums.get(j-1)).sum();
-            System.out.println(sum + " " + maxSum);
-            if (sum>maxSum) {
-                System.out.println(Arrays.toString(subset));
-            }
+            // System.out.println(sum + " " + maxSum);
+            // if (sum>maxSum) {
+            //     System.out.println(Arrays.toString(subset));
+            // }
             maxSum = Math.max(maxSum, sum);
             // int a = subset[0];
             // int b = subset[2];
@@ -29,7 +27,7 @@ class Solution {
             builder.add(init*i*i);
         }
         int[] subset = builder.build().toArray();
-        System.out.println(Arrays.toString(subset));
+        // System.out.println(Arrays.toString(subset));
         return subset;
     }
 
