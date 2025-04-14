c8eceb2ab36a93b255f5a2304c71ee0cf2813eec
cnux9 <65856226+cnux9@users.noreply.github.com>
2025-04-14 23:13:51 +0900

    Time: 1 ms (76.26%), Space: 41.5 MB (7.59%) - LeetHub

---
+++ 2894-divisible-and-non-divisible-sums-difference/2894-divisible-and-non-divisible-sums-difference.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int differenceOfSums(int n, int m) {
+        int result = 0;
+        for (int i = 1; i <= n; i++) {
+            result += (i % m == 0) ? -i : i;
+        }
+        return result;
+    }
+}
