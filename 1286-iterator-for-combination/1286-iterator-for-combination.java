8ea83c97bd332406d745d8edfd6babf5555630b8
cnux9 <65856226+cnux9@users.noreply.github.com>
2025-04-21 19:15:41 +0900

    Time: 66 ms (6.31%), Space: 47.7 MB (6.31%) - LeetHub

---
+++ 1286-iterator-for-combination/1286-iterator-for-combination.java
@@ -0,0 +1,57 @@
+// import java.util.stream.IntStream;
+
+class CombinationIterator {
+    int[] charactersArray;
+    int[] indexArray;
+    boolean hasNext = true;
+
+    public CombinationIterator(String characters, int combinationLength) {
+        this.charactersArray = characters.chars().sorted().toArray();
+
+        // this.characters = characters;
+        this.indexArray = IntStream.range(0, combinationLength).toArray();
+    }
+    
+    public String next() {
+        if (!this.hasNext) {
+            return null;
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < indexArray.length; i++) {
+            sb.append((char) charactersArray[indexArray[i]]);
+        }
+
+        System.out.println(sb.toString());
+        hasNext = false;
+        for (int i = indexArray.length-1; i >= 0; i--) {
+            System.out.println("loop " + i);
+            if (indexArray[i] != charactersArray.length - (indexArray.length - i)) {
+                indexArray[i]++;
+                for (int j = 1; j + i < indexArray.length; j++) {
+                    System.out.println("    loop " + i + " : " + j);
+                    indexArray[j + i] = indexArray[i] + j;
+                    System.out.println("        " + indexArray[j + i]);
+                }
+                hasNext = true;
+                break;
+            }
+        }
+        // if (hasNext == false) {
+        //     return null;
+        // }
+        
+        System.out.println("loop end ");
+        return sb.toString();
+    }
+    
+    public boolean hasNext() {
+        return this.hasNext;
+    }
+}
+
+/**
+ * Your CombinationIterator object will be instantiated and called as such:
+ * CombinationIterator obj = new CombinationIterator(characters, combinationLength);
+ * String param_1 = obj.next();
+ * boolean param_2 = obj.hasNext();
+ */
