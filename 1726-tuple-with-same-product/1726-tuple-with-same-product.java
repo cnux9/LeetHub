5cad5642c641a6d57dd8bf4c95f2e8b4936c3ffd
cnux9 <65856226+cnux9@users.noreply.github.com>
2025-04-26 20:48:09 +0900

    Time: 1110 ms (5.09%), Space: 41.9 MB (98.91%) - LeetHub

--- 1726-tuple-with-same-product/1726-tuple-with-same-product.java
+++ 1726-tuple-with-same-product/1726-tuple-with-same-product.java
@@ -7,9 +7,9 @@ class Solution {
                 for (int i3 = i2 + 1; i3 < nums.length; i3++) {
                     if (nums[i2] * nums[i3] % nums[i1] == 0) {
                         int target = nums[i2] * nums[i3] / nums[i1];
-                        if (target <= nums[nums.length-1]) {
+                        if (nums[i3] < target && target <= nums[nums.length-1]) {
                             int pos = Arrays.binarySearch(nums, i3 + 1, nums.length, target);
-                            if (pos > 0) {
+                            if (pos > -1) {
                                 count++;
                             }
                         }
