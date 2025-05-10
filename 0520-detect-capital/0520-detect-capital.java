1f5836a2d11e9c548befde69ff68828e05bf9571
cnux9 <65856226+cnux9@users.noreply.github.com>
2025-05-10 15:46:49 +0900

    Time: 1 ms (92.3%), Space: 41.8 MB (76.53%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean detectCapitalUse(String word) {
+        if (word.length() == 1) {
+            return true;
+        }
+        if (Character.isUpperCase(word.charAt(0))) {
+            boolean target = Character.isUpperCase(word.charAt(1));
+            for (int i = 2; i < word.length(); i++) {
+                if (Character.isUpperCase(word.charAt(i)) != target) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        for (int i = 1; i < word.length(); i++) {
+            if (Character.isUpperCase(word.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
