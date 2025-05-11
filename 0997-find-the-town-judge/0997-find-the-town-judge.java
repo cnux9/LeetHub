bfc3f0525d0ee7793ae5f532f5d938f1185db8a6
cnux9 <65856226+cnux9@users.noreply.github.com>
2025-05-11 23:41:37 +0900

    Time: 3 ms (65.62%), Space: 54.3 MB (25.16%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.java
+++ 0997-find-the-town-judge/0997-find-the-town-judge.java
@@ -1,18 +1,16 @@
 class Solution {
     public int findJudge(int n, int[][] trust) {
         int[] followers = new int[n];
-        boolean[] hasFollowing = new boolean[n];
         Arrays.fill(followers, 0);
-        Arrays.fill(hasFollowing, false);
 
         for (int[] rel : trust) {
-            hasFollowing[rel[0]-1] = true;
+            followers[rel[0]-1]--;
             followers[rel[1]-1]++;
         }
 
 
         for (int i = 0; i < n; i++) {
-            if (!hasFollowing[i] && followers[i]==n-1) {
+            if (followers[i]==n-1) {
                 return i+1;
             }
         }
