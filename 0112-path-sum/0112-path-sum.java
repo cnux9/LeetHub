282486b0ee1856e5a6340c088a750d3db9130dd7
cnux9 <65856226+cnux9@users.noreply.github.com>
2025-05-14 00:08:19 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0112-path-sum/0112-path-sum.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) {
+            return false;
+        }
+        return hasPathSumInternal(root, targetSum);
+    }
+
+    private boolean hasPathSumInternal(TreeNode root, int targetSum) {
+        System.out.println(root.val);
+        if (targetSum == root.val && root.right == null && root.left == null) {
+            return true;
+        }
+        if (targetSum < root.val) {
+            return false;
+        }
+        if (root.right != null && hasPathSum(root.right, targetSum-root.val)) {
+            return true;
+        }
+        if (root.left != null && hasPathSum(root.left, targetSum-root.val)) {
+            return true;
+        }
+        return false;
+    }
+}
