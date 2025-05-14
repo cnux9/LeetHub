1216bfd2040098aaec14c6163bda701e6a2fcf40
cnux9 <65856226+cnux9@users.noreply.github.com>
2025-05-14 23:51:10 +0900

    Time: 39 ms (7.67%), Space: 46.1 MB (5.57%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String addBinary(String a, String b) {
+        if (a.length() > b.length()) {
+            String temp = b;
+            b = a;
+            a = temp;
+        }
+
+        int next = 0;
+        String result = "";
+        for (int i = 0; i < b.length(); i++) {
+            System.out.println(result + " : " + next + " : " + i);
+            int cursor = Character.getNumericValue(b.charAt(b.length() - i - 1)) + next;
+            next = 0;
+            if (i <= a.length() - 1) {
+                cursor += Character.getNumericValue(a.charAt(a.length() - i - 1));
+            }
+            if (cursor >= 2) {
+                next = 1;
+                cursor -= 2;
+            }
+            result = cursor + result;
+        }
+        if (next == 1) {
+            result = next + result;
+        }
+        return result;
+    }
+}
