e0074ac0457086981c4e47adffe5af5841588e8f
cnux9 <65856226+cnux9@users.noreply.github.com>
2025-05-21 00:02:22 +0900

    Time: 3 ms (36.1%), Space: 41.9 MB (75.03%) - LeetHub

--- 2016-maximum-difference-between-increasing-elements/2016-maximum-difference-between-increasing-elements.java
+++ 2016-maximum-difference-between-increasing-elements/2016-maximum-difference-between-increasing-elements.java
@@ -1,7 +1,19 @@
 class Solution {
     public int maximumDifference(int[] nums) {
+        List numList = new ArrayList();
+
+        if (nums[0] > nums[1]) {
+
+        }
+        for (int i = 2; i < nums.length; i++) {
+
+        }
+
         int max = 0;
         for (int i = 0; i < nums.length; i++) {
+            if (i != 0 && nums[i-1] <= nums[i]) {
+                continue;
+            }
             for (int j = i+1; j < nums.length; j++) {
                 int val = nums[j] - nums[i];
                 if (max < val) {
