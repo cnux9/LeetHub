c36291cb86d4e6fa9fa79eb366bfef9b708968fd
cnux9 <65856226+cnux9@users.noreply.github.com>
2025-05-28 23:22:04 +0900

    Time: 21 ms (3.53%), Space: 43.1 MB (5.58%) - LeetHub

--- 2873-maximum-value-of-an-ordered-triplet-i/2873-maximum-value-of-an-ordered-triplet-i.java
+++ 2873-maximum-value-of-an-ordered-triplet-i/2873-maximum-value-of-an-ordered-triplet-i.java
@@ -4,10 +4,12 @@ class Solution {
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 for (int k = j + 1; k < nums.length; k++) {
-                    long val = ((long) nums[i] - nums[j]) * nums[k];
-                    if (max < val) {
-                        System.out.println(i + " " + j + " " + k);
-                        max = val;
+                    if (nums[i] > nums[j]) {
+                        long val = ((long) nums[i] - nums[j]) * nums[k];
+                        if (max < val) {
+                            System.out.println(i + " " + j + " " + k);
+                            max = val;
+                        }
                     }
                 }
             }
