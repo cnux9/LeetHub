da175c5113df8127b7aa1875bb95910466c8167e
cnux9 <65856226+cnux9@users.noreply.github.com>
2025-06-02 14:45:41 +0900

    Time: 14 ms (68.87%), Space: 43.1 MB (51.39%) - LeetHub

--- 0819-most-common-word/0819-most-common-word.java
+++ 0819-most-common-word/0819-most-common-word.java
@@ -2,12 +2,14 @@ class Solution {
     public String mostCommonWord(String paragraph, String[] banned) {
         Map<String, Integer> dict = new HashMap<>();
         String[] words = paragraph.split("[!\\?,';\\.\\s]+");
+        List bannedList = Arrays.asList(banned);
         for (String word : words) {
             word = word.toLowerCase();
-            dict.put(word, dict.getOrDefault(word, 0) + 1);
+            if (!bannedList.contains(word)) {
+                dict.put(word, dict.getOrDefault(word, 0) + 1);
+            }
         }
-        List bannedList = Arrays.asList(banned);
-        String result = dict.keySet().stream().filter(k -> !bannedList.contains(k)).max(Comparator.comparingInt(k -> dict.get(k))).orElse("");
+        String result = dict.keySet().stream().max(Comparator.comparingInt(k -> dict.get(k))).orElse("");
         return result;
     }
 }
