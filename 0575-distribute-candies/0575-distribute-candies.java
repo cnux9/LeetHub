e1d686965c8ff1f634becbff1c964632368157de
cnux9 <65856226+cnux9@users.noreply.github.com>
2025-06-03 23:12:39 +0900

    Time: 20 ms (96.16%), Space: 46 MB (30.81%) - LeetHub

--- 0575-distribute-candies/0575-distribute-candies.java
+++ 0575-distribute-candies/0575-distribute-candies.java
@@ -3,6 +3,9 @@ class Solution {
         Set<Integer> candySet = new HashSet<>();
         for (int t : candyType) {
             candySet.add(t);
+            if (candySet.size() >= candyType.length / 2) {
+                return candyType.length / 2;
+            }
         }
         return Math.min(candySet.size(), candyType.length / 2);
     }
