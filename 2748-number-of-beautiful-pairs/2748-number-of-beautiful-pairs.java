21e17951a4ecb94666b718aea1783553cb4e3dde
cnux9 <65856226+cnux9@users.noreply.github.com>
2025-06-04 21:30:27 +0900

    Time: 15 ms (55%), Space: 45.2 MB (17.08%) - LeetHub

--- 2748-number-of-beautiful-pairs/2748-number-of-beautiful-pairs.java
+++ 2748-number-of-beautiful-pairs/2748-number-of-beautiful-pairs.java
@@ -1,27 +1,27 @@
 class Solution {
     public int countBeautifulPairs(int[] nums) {
-        int[][] digits = new int[nums.length][2];
+        int[] primes = new int[] {2, 3, 5, 7};
+
+        boolean[][][] digits = new boolean[nums.length][2][4];
         for (int i = 0; i < nums.length; i++) {
-            digits[i][0] = nums[i];
-            while (digits[i][0] >= 10) {
-                digits[i][0] /= 10;
+            int val1 = nums[i];
+            while (val1 >= 10) {
+                val1 /= 10;
             }
-            digits[i][1] = nums[i] % 10;
+            int val2 = nums[i] % 10;
+
+            for(int j = 0; j < primes.length; j++) {
+                digits[i][0][j] = (val1 % primes[j] == 0);
+                digits[i][1][j] = (val2 % primes[j] == 0);
+            }
         }
 
-        int[] primes = new int[] {2, 3, 5, 7};
         int count = 0;
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
-                // System.out.println("pow: " + Math.pow(10, (int)Math.log10(nums[i])));
-                int val1 = digits[i][0];
-                int val2 = digits[j][1];
-
-                // System.out.println(val1 + " " + val2);
                 boolean isCoprime = true;
-                for (int p : primes) {
-                    if (val1 % p == 0 && val2 % p == 0) {
-                        // System.out.println(val1 + " " + val2 + " " + p + " suc!");
+                for (int k = 0; k < digits[0][0].length; k++) {
+                    if (digits[i][0][k] && digits[j][1][k]) {
                         isCoprime = false;
                         break;
                     }
