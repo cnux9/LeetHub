e24e43b79f27e4c7dbf7b9e3271b79a9179d8f75
cnux9 <65856226+cnux9@users.noreply.github.com>
2025-06-04 21:15:37 +0900

    Time: 16 ms (50%), Space: 45.5 MB (8.33%) - LeetHub

--- 2748-number-of-beautiful-pairs/2748-number-of-beautiful-pairs.java
+++ 2748-number-of-beautiful-pairs/2748-number-of-beautiful-pairs.java
@@ -1,12 +1,18 @@
 class Solution {
     public int countBeautifulPairs(int[] nums) {
+        int[][] digits = new int[nums.length][2];
+        for (int i = 0; i < nums.length; i++) {
+            digits[i][0] = nums[i] / (int)Math.pow(10, (int)Math.log10(nums[i]));
+            digits[i][1] = nums[i] % 10;
+        }
+
         int[] primes = new int[] {2, 3, 5, 7};
         int count = 0;
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 // System.out.println("pow: " + Math.pow(10, (int)Math.log10(nums[i])));
-                int val1 = nums[i] / (int)Math.pow(10, (int)Math.log10(nums[i]));
-                int val2 = nums[j] % 10;
+                int val1 = digits[i][0];
+                int val2 = digits[j][1];
 
                 // System.out.println(val1 + " " + val2);
                 boolean isCoprime = true;
