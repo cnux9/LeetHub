39f4f31b8cedea43c6399abf9e77d0e296a12d5b
cnux9 <65856226+cnux9@users.noreply.github.com>
2025-06-15 23:36:49 +0900

    Time: 8 ms (8.15%), Space: 45.1 MB (6.52%) - LeetHub

---
+++ 3216-lexicographically-smallest-string-after-a-swap/3216-lexicographically-smallest-string-after-a-swap.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String getSmallestString(String s) {
+        String min = s;
+        for (int i = 0; i < s.length() - 1; i++) {
+            if (s.charAt(i) % 2 != s.charAt(i + 1) % 2) {
+                continue;
+            }
+            StringBuilder sb = new StringBuilder();
+            for (int j = 0; j < s.length(); j++) {
+                if (j == i) {
+                    sb.append(s.charAt(i + 1));
+                } else if (j == i + 1) {
+                    sb.append(s.charAt(i));
+                } else {
+                    sb.append(s.charAt(j));
+                }
+            }
+            String val = sb.toString();
+            if (val.compareTo(min) < 0) {
+                min = val;
+            }
+        }
+        return min;
+    }
+}
