099ff3998d900cd9784c292cb1f90962a3c5e257
cnux9 <65856226+cnux9@users.noreply.github.com>
2025-06-16 11:31:55 +0900

    Time: 3 ms (16.76%), Space: 42.9 MB (50.81%) - LeetHub

--- 3216-lexicographically-smallest-string-after-a-swap/3216-lexicographically-smallest-string-after-a-swap.java
+++ 3216-lexicographically-smallest-string-after-a-swap/3216-lexicographically-smallest-string-after-a-swap.java
@@ -1,25 +1,22 @@
 class Solution {
     public String getSmallestString(String s) {
-        String min = s;
         for (int i = 0; i < s.length() - 1; i++) {
-            if (s.charAt(i) % 2 != s.charAt(i + 1) % 2 || s.charAt(i) < s.charAt(i + 1)) {
+            if (s.charAt(i) % 2 != s.charAt(i + 1) % 2 || s.charAt(i) < s.charAt(i + 1) || s.charAt(i) == s.charAt(i + 1)) {
                 continue;
             }
+            System.out.println("" + i);
             StringBuilder sb = new StringBuilder();
-            for (int j = 0; j < s.length(); j++) {
-                if (j == i) {
-                    sb.append(s.charAt(i + 1));
-                } else if (j == i + 1) {
-                    sb.append(s.charAt(i));
-                } else {
-                    sb.append(s.charAt(j));
-                }
+            for (int j = 0; j < i; j++) {
+                sb.append(s.charAt(j));
+            }
+            sb.append(s.charAt(i + 1));
+            sb.append(s.charAt(i));
+            for (int j = i + 2; j < s.length(); j++) {
+                sb.append(s.charAt(j));
             }
             String val = sb.toString();
-            if (val.compareTo(min) < 0) {
-                return val;
-            }
+            return val;
         }
-        return min;
+        return s;
     }
 }
