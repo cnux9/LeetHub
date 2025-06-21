76c58c1ba7d8f3662d22e5129d1cc2f5d5261461
cnux9 <65856226+cnux9@users.noreply.github.com>
2025-06-21 23:55:25 +0900

    Time: 0 ms (100%), Space: 41.6 MB (93.65%) - LeetHub

--- 2259-remove-digit-from-number-to-maximize-result/2259-remove-digit-from-number-to-maximize-result.java
+++ 2259-remove-digit-from-number-to-maximize-result/2259-remove-digit-from-number-to-maximize-result.java
@@ -2,27 +2,17 @@ class Solution {
     char[] charArray;
 
     public String removeDigit(String number, char digit) {
-        boolean isRemoved = false;
-        List<Integer> indices = new ArrayList<>();
         charArray = number.toCharArray();
+        int lastIndex = 0;
         for (int i = 0; i < charArray.length; i++) {
             if (charArray[i] == digit) {
-                if (i != charArray.length-1 && charArray[i+1] > digit) {
+                if (i != charArray.length - 1 && charArray[i+1] > digit) {
                     return getRemovedString(i);
                 }
-                indices.add(i);
+                lastIndex = i;
             }
         }
-
-        
-        String max = "";
-        for (int i : indices) {
-             String val = getRemovedString(i);
-             if (max.compareTo(val) < 0) {
-                max = val;
-             }
-        }
-        return max + "";
+        return getRemovedString(lastIndex);
     }
 
     private String getRemovedString(int index) {
