13d9f7752715c7bccdd0d4d128f12c785c4cc5eb
cnux9 <65856226+cnux9@users.noreply.github.com>
2025-07-03 23:22:15 +0900

    Time: 1 ms (66.11%), Space: 40.8 MB (51.98%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isHappy(int n) {
+        int result = n;
+
+        int test_count = 0;
+        while (result != 1) {
+            int sum = 0;
+            while (result > 0) {
+                int digit = result % 10;
+                sum += digit * digit;
+                result /= 10;
+            }
+            result = sum;
+
+            if (test_count == 100) {
+                return false;
+            }
+            test_count++;
+        }
+        return true;
+    }
+}
