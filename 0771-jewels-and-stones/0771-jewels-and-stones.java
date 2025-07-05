685c9dcd61845af6cf4fb9d05e37a65eeca2f472
cnux9 <65856226+cnux9@users.noreply.github.com>
2025-07-05 23:29:52 +0900

    Time: 1 ms (58.42%), Space: 41.9 MB (61.58%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.java
+++ 0771-jewels-and-stones/0771-jewels-and-stones.java
@@ -3,10 +3,20 @@ class Solution {
         char[] chars = jewels.toCharArray();
         Arrays.sort(chars);
         int count = 0;
+        char prev = '-';
+        boolean wasCounted = false;
         for (char c : stones.toCharArray()) {
+            if (c == prev && wasCounted) {
+                count++;
+                continue;
+            }
             if (Arrays.binarySearch(chars, c) >= 0) {
                 count++;
+                wasCounted = true;
+            } else {
+                wasCounted = false;
             }
+            prev = c;
         }
         return count;
     }
