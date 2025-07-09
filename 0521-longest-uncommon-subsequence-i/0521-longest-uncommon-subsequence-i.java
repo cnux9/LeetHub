91d5449b15cd9a11bfdd9d826b13a3749d939fe1
cnux9 <65856226+cnux9@users.noreply.github.com>
2025-07-09 23:21:48 +0900

    Time: 2 ms (1.94%), Space: 42.5 MB (9.83%) - LeetHub

--- 0521-longest-uncommon-subsequence-i/0521-longest-uncommon-subsequence-i.java
+++ 0521-longest-uncommon-subsequence-i/0521-longest-uncommon-subsequence-i.java
@@ -6,21 +6,7 @@ class Solution {
         int length = a.length();
         for (int l = length - 1; l > 0; l--) {
             for (int i = 0; i < length - l; i++) {
-                boolean hasSubsequence = false;
-                for (int j = 0; j < length - l; j++) {
-                    boolean isSame = true;
-                    for (int k = 0; k <= l; k++) {
-                        if (a.charAt(i + k) != b.charAt(j + k)) {
-                            isSame = false;
-                            break;
-                        }
-                    }
-                    if (isSame) {
-                        hasSubsequence = true;
-                        break;
-                    }
-                }
-                if (!hasSubsequence) {
+                if (!b.contains(a.substring(i, i + l))) {
                     return l + 1;
                 }
             }
