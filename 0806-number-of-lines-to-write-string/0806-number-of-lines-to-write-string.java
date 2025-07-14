33985902323a91fbc8c5bee20857b87d864360e1
cnux9 <65856226+cnux9@users.noreply.github.com>
2025-07-14 23:09:45 +0900

    Time: 36 ms (2.07%), Space: 45.2 MB (7.67%) - LeetHub

---
+++ 0806-number-of-lines-to-write-string/0806-number-of-lines-to-write-string.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] numberOfLines(int[] widths, String s) {
+        System.out.println(s.length());
+        int count = 0;
+        int lineCount = 1;
+        for (int i = 0; i < s.length(); i++) {
+            System.out.println(i + " " + String.valueOf(s.charAt(i)) + " " + count + " " + lineCount);
+            // System.out.println(i + " " + count + " " + lineCount);
+            int ascii = (int)s.charAt(i) - 97;
+
+            // System.out.println(ascii);
+            count += widths[ascii];
+            if (count > 100) {
+                lineCount++;
+                count = widths[ascii];
+            }
+        }
+        return new int[] {lineCount, count};
+    }
+}
