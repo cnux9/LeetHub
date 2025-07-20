9c59724947689c92262b48dd8728d6d7e3c39946
cnux9 <65856226+cnux9@users.noreply.github.com>
2025-07-20 23:18:21 +0900

    Time: 52 ms (9.4%), Space: 42.5 MB (47.86%) - LeetHub

--- 3411-maximum-subarray-with-equal-products/3411-maximum-subarray-with-equal-products.java
+++ 3411-maximum-subarray-with-equal-products/3411-maximum-subarray-with-equal-products.java
@@ -1,23 +1,21 @@
 class Solution {
-    int[] primes = new int[] {2, 3, 5, 7};
-    int[][] primeArr;
-
     public int maxLength(int[] nums) {
-        primeArr = new int[nums.length][];
-
-        for (int i = 0; i < nums.length; i++) {
-            primeArr[i] = getPrimes(nums[i]);
-        }
-
         for (int len = nums.length; len > 0; len--) {
-            int[] indices = new int[len];
-            for (int i = 0; i < indices.length; i++) {
-                indices[i] = i;
-            }
-
+            
+            // System.out.println("" + len);
             for (int i = 0; i + len - 1 < nums.length; i++) {
-                if (isValidArr(i, i + len)) {
-                    // System.out.println("valid!");
+                long product = (long)nums[i];
+                int gcdNum = nums[i];
+                int lcmNum = nums[i];
+                for (int j = 1; j < len; j++) {
+                    // System.out.println("    " + i + " " + j);
+                    product *= nums[i + j];
+                    gcdNum = gcd(gcdNum, nums[i + j]);
+                    lcmNum = lcm(lcmNum, nums[i + j]);
+                }
+                
+                // System.out.println(product + " " + gcdNum + " " + lcmNum + "\n");
+                if (product == (long) gcdNum * lcmNum) {
                     return len;
                 }
 
@@ -26,67 +24,11 @@ class Solution {
         return -1;
     }
 
-    private boolean isValidArr(int start, int end) {
-        // System.out.println("isValidArr: " + start + ", " + end);
-
-        int product = 1;
-
-        int[] countArr = new int[] {0, 0, 0, 0};
-        int[] gcdArr = new int[] {4, 4, 4, 4};
-        int[] lcmArr = new int[] {0, 0, 0, 0};
-        for (int i = start; i < end; i++) {
-            int[] returnedPrimes = primeArr[i];
-            // System.out.println("returnedPrimes: " + n + " -> " + Arrays.toString(returnedPrimes));
-
-            for (int j = 0; j < countArr.length; j++) {
-                countArr[j] += returnedPrimes[j];
-                // if (countArr[j] > 2) {
-                //     return false;
-                // }
-
-                // gcd
-                if (gcdArr[j] > returnedPrimes[j]) {
-                    gcdArr[j] = returnedPrimes[j];
-                }
-
-                // lcm
-                if (lcmArr[j] < returnedPrimes[j]) {
-                    lcmArr[j] = returnedPrimes[j];
-                }
-            }
-        }
-
-        // System.out.println("countArr: " + Arrays.toString(countArr));
-        // System.out.println("gcdArr: " + Arrays.toString(gcdArr));
-        // System.out.println("lcmArr: " + Arrays.toString(lcmArr));
-
-        for (int i = 0; i < primes.length; i++) {
-            if (gcdArr[i] != 4) {
-                lcmArr[i] += gcdArr[i];
-            }
-            if (lcmArr[i] != countArr[i]) {
-                return false;
-            }
-        }
-
-        return true;
+    private int gcd(int a, int b) {
+        return (b == 0)?  a : gcd(b, a % b);
     }
 
-    // 8 -> {3, 0, 0, 0}
-    private int[] getPrimes(int n) {
-        int[] countArr = new int[] {0, 0, 0, 0};
-
-        int q = n;
-        int i = 0;
-        while (q > 1) {
-            int p = primes[i];
-            if (q % p == 0) {
-                countArr[i]++;
-                q /= p;
-            } else {
-                i++;
-            }
-        }
-        return countArr;
+    private int lcm(int a, int b) {
+        return (a * b) / gcd(a, b);
     }
 }
