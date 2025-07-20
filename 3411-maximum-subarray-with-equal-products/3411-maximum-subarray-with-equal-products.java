59ae712d6a3d6df53de1ea3175448f1b0f49f2c0
cnux9 <65856226+cnux9@users.noreply.github.com>
2025-07-20 23:56:56 +0900

    Time: 161 ms (5.13%), Space: 45.2 MB (5.13%) - LeetHub

--- 3411-maximum-subarray-with-equal-products/3411-maximum-subarray-with-equal-products.java
+++ 3411-maximum-subarray-with-equal-products/3411-maximum-subarray-with-equal-products.java
@@ -1,34 +1,88 @@
 class Solution {
-    public int maxLength(int[] nums) {
-        for (int len = nums.length; len > 0; len--) {
-            
-            // System.out.println("" + len);
-            for (int i = 0; i + len - 1 < nums.length; i++) {
-                long product = (long)nums[i];
-                int gcdNum = nums[i];
-                int lcmNum = nums[i];
-                for (int j = 1; j < len; j++) {
-                    // System.out.println("    " + i + " " + j);
-                    product *= nums[i + j];
-                    gcdNum = gcd(gcdNum, nums[i + j]);
-                    lcmNum = lcm(lcmNum, nums[i + j]);
-                }
+    int[] nums;
+
+    private class Node {
+        Node left, right;
+        int start, end;
+        long product, gcd, lcm;
+
+        Node(int start, int end) {
+            this.start = start;
+            this.end = end;
+            if (end - start != 1) {
+                // System.out.println("1");
+                int mid = (start + end) / 2;
+                this.left = new Node(start, mid);
+                this.right = new Node(mid, end);
                 
-                // System.out.println(product + " " + gcdNum + " " + lcmNum + "\n");
-                if (product == (long) gcdNum * lcmNum) {
+                this.product = this.left.product * this.right.product;
+                this.gcd = getGcd(this.left.gcd, this.right.gcd);
+                this.lcm = getLcm(this.left.lcm, this.right.lcm);
+            } else {
+                this.left = null;
+                this.right = null;
+                this.product = nums[start];
+                this.gcd = nums[start];
+                this.lcm = nums[start];
+            }
+        }
+
+        public boolean isValid(int start, int end) {
+            // System.out.println("called node is valid: " + this.start + ", " + this.end);
+            if (this.start >= end || this.end <= start) {
+                return false;
+            }
+            return true;
+        }
+
+        public long[] getValues(int start, int end) {
+            // System.out.println("called node: " + this.start + ", " + this.end);
+            if (this.left == null) {
+                return new long[] {this.product, this. gcd, this.lcm};
+            }
+            // System.out.println("1");
+            boolean isLeftValid = this.left.isValid(start, end);
+            boolean isRightValid = this.right.isValid(start, end);
+            if (isLeftValid && isRightValid) {
+                return getValuesOtTwo(this.left.getValues(start, end), this.right.getValues(start, end));
+            }
+            if (isLeftValid) {
+                return this.left.getValues(start, end);
+            }
+            return this.right.getValues(start, end);
+        }
+    }
+
+    public int maxLength(int[] nums) {
+        this.nums = nums;
+        Node root = new Node(0, nums.length);
+        for (int len = nums.length; len > 0; len--) {
+            for (int i = 0; i + len - 1 < nums.length; i++) {
+                    
+                long[] arr = root.getValues(i, i + len);
+                // System.out.println(Arrays.toString(arr));
+                if (arr[0] == arr[1] * arr[2]) {
                     return len;
                 }
-
             }
         }
         return -1;
     }
 
-    private int gcd(int a, int b) {
-        return (b == 0)?  a : gcd(b, a % b);
+    private long getGcd(long a, long b) {
+        return (b == 0)?  a : getGcd(b, a % b);
     }
 
-    private int lcm(int a, int b) {
-        return (a * b) / gcd(a, b);
+    private long getLcm(long a, long b) {
+        return (a * b) / getGcd(a, b);
+    }
+
+    private long[] getValuesOtTwo(long[] a, long[] b) {
+        long[] arr = new long[] {
+            a[0] * b[0],
+            getGcd(a[1], b[1]),
+            getLcm(a[2], b[2])
+        };
+        return arr;
     }
 }
