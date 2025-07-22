8219892363bdb24cac9c18b667de9c6a856c500a
cnux9 <65856226+cnux9@users.noreply.github.com>
2025-07-22 16:15:28 +0900

    Time: 18 ms (14.89%), Space: 45.2 MB (5.96%) - LeetHub

--- 3411-maximum-subarray-with-equal-products/3411-maximum-subarray-with-equal-products.java
+++ 3411-maximum-subarray-with-equal-products/3411-maximum-subarray-with-equal-products.java
@@ -25,7 +25,6 @@ class Solution {
 
             for (int i = 0; i + len - 1 < nums.length; i++) {
                 if (isValidArr(i, i + len)) {
-                    // System.out.println("valid!");
                     return len;
                 }
 
@@ -35,23 +34,12 @@ class Solution {
     }
 
     private boolean isValidArr(int start, int end) {
-        // System.out.println();
-        // System.out.println("isValidArr: " + start + ", " + end);
 
         int product = 1;
 
         int[] countArr = new int[] {0, 0, 0, 0};
         int[][] countExpArr = new int[][] {{0, 0, 0, 0}, {0, 0, 0}, {0, 0}, {0, 0}};
-        int[] gcdArr = new int[] {4, 4, 4, 4};
-        int[] lcmArr = new int[] {0, 0, 0, 0};
         for (int i = start; i < end; i++) {
-            // if (nums[i] == 1) {
-            //     gcdArr[0] = 0;
-            //     gcdArr[1] = 0;
-            //     gcdArr[2] = 0;
-            //     gcdArr[3] = 0;
-            //     continue;
-            // }
             int[] returnedPrimes = PRIME_EXP[nums[i] - 1];
             // System.out.println("returnedPrimes: " + n + " -> " + Arrays.toString(returnedPrimes));
 
@@ -63,44 +51,8 @@ class Solution {
                     // System.out.println("=========" + start + ", " + end);
                     return false;
                 }
-
-                // gcd
-                if (gcdArr[j] > returnedPrimes[j]) {
-                    gcdArr[j] = returnedPrimes[j];
-                }
-
-                // lcm
-                if (lcmArr[j] < returnedPrimes[j]) {
-                    lcmArr[j] = returnedPrimes[j];
-                }
             }
         }
-
-        // System.out.println("countArr: " + Arrays.toString(countArr));
-        // System.out.println("gcdArr: " + Arrays.toString(gcdArr));
-        // System.out.println("lcmArr: " + Arrays.toString(lcmArr));
-
-        // System.out.println("=========" + start + ", " + end);
-        // if (end - start == 2) {
-        //     return true;
-        // } else {
-        //     for (int p : gcdArr) {
-        //         System.out.println("gcd : " + p);
-        //         if (p != 0 && p != 4) {
-        //             System.out.println("====prime is not valid");
-        //             return false;
-        //         }
-        //     }
-        //     System.out.println();
-        //     for (int p : lcmArr) {
-        //         System.out.println("lcm : " + p);
-        //         if (p != 0 && p != 1) {
-        //             System.out.println("====prime is not valid");
-        //             return false;
-        //         }
-        //     }
-        //     System.out.println();
-        // }
         
         for (int i = 0; i < countArr.length; i++) {
 
@@ -131,21 +83,12 @@ class Solution {
                         return false;
                     }
                 }
-
-                // return false;
             }
             if (count == 1 && countExpArr[i][0] == 0 && countAll != 2) {
                 System.out.println("false3 : " + start + ", " + end);
 
                 return false;
             }
-
-            // if (gcdArr[i] != 4) {
-            //     lcmArr[i] += gcdArr[i];
-            // }
-            // if (lcmArr[i] != countArr[i]) {
-            //     return false;
-            // }
         }
 
         return true;
