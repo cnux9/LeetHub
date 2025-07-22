8a0c4915662625e8c7e30cf7d8dc76948a780653
cnux9 <65856226+cnux9@users.noreply.github.com>
2025-07-22 13:59:03 +0900

    Time: 49 ms (11.91%), Space: 45.2 MB (5.96%) - LeetHub

--- 3411-maximum-subarray-with-equal-products/3411-maximum-subarray-with-equal-products.java
+++ 3411-maximum-subarray-with-equal-products/3411-maximum-subarray-with-equal-products.java
@@ -1,117 +1,94 @@
 class Solution {
-    private static final char GL = 11;
-    private static final short LL = 2521;
-
-    int[] nums;
-
-    private class Node {
-        Node left, right;
-        int start, end;
-        long val;
-
-        Node(int start, int end) {
-            this.start = start;
-            this.end = end;
-            if (end - start != 1) {
-                // System.out.println("1");
-                int mid = (start + end) / 2;
-                this.left = new Node(start, mid);
-                this.right = new Node(mid, end);
-                
-                long temp = this.left.val;
-                long lp = temp / (GL * LL);
-                temp %= (GL * LL);
-                long lg = temp / LL;
-                long ll = temp % LL;
-
-                temp = this.right.val;
-                long rp = temp / (GL * LL);
-                temp %= (GL * LL);
-                long rg = temp / LL;
-                long rl = temp % LL;
-
-                this.val = lp * rp;
-                this.val = this.val * GL + getGcd(lg, rg);
-                this.val = this.val * LL + getLcm(ll, rl);
-            } else {
-                this.left = null;
-                this.right = null;
-                this.val = nums[start];
-                this.val = this.val * GL + nums[start];
-                this.val = this.val * LL + nums[start];
-            }
-        }
-
-        public boolean isValid(int start, int end) {
-            // System.out.println("called node is valid: " + this.start + ", " + this.end);
-            if (this.start >= end || this.end <= start) {
-                return false;
-            }
-            return true;
-        }
-
-        public long getValues(int start, int end) {
-            // System.out.println("called node: " + this.start + ", " + this.end);
-            if (this.left == null) {
-                return this.val;
-            }
-            // System.out.println("1");
-            boolean isLeftValid = this.left.isValid(start, end);
-            boolean isRightValid = this.right.isValid(start, end);
-            if (isLeftValid && isRightValid) {
-                return getValuesOtTwo(this.left.getValues(start, end), this.right.getValues(start, end));
-            }
-            if (isLeftValid) {
-                return this.left.getValues(start, end);
-            }
-            return this.right.getValues(start, end);
-        }
-    }
+    int[] primes = new int[] {2, 3, 5, 7};
+    int[][] primeArr;
 
     public int maxLength(int[] nums) {
-        this.nums = nums;
-        Node root = new Node(0, nums.length);
-        for (int len = nums.length; len > 0; len--) {
-            for (int i = 0; i + len - 1 < nums.length; i++) {
-                    
-                long val = root.getValues(i, i + len);
-                // System.out.println(Arrays.toString(arr));
-                        
-                long p = val / (GL * LL);
-                val %= (GL * LL);
-                long g = val / LL;
-                long l = val % LL;
+        primeArr = new int[nums.length][];
 
-                if (p == g * l) {
+        for (int i = 0; i < nums.length; i++) {
+            primeArr[i] = getPrimes(nums[i]);
+        }
+
+        for (int len = nums.length; len > 0; len--) {
+            int[] indices = new int[len];
+            for (int i = 0; i < indices.length; i++) {
+                indices[i] = i;
+            }
+
+            for (int i = 0; i + len - 1 < nums.length; i++) {
+                if (isValidArr(i, i + len)) {
+                    // System.out.println("valid!");
                     return len;
                 }
+
             }
         }
         return -1;
     }
 
-    private long getGcd(long a, long b) {
-        return (b == 0)?  a : getGcd(b, a % b);
+    private boolean isValidArr(int start, int end) {
+        // System.out.println("isValidArr: " + start + ", " + end);
+
+        int product = 1;
+
+        int[] countArr = new int[] {0, 0, 0, 0};
+        int[] gcdArr = new int[] {4, 4, 4, 4};
+        int[] lcmArr = new int[] {0, 0, 0, 0};
+        for (int i = start; i < end; i++) {
+            int[] returnedPrimes = primeArr[i];
+            // System.out.println("returnedPrimes: " + n + " -> " + Arrays.toString(returnedPrimes));
+
+            for (int j = 0; j < countArr.length; j++) {
+                countArr[j] += returnedPrimes[j];
+                // if (countArr[j] > 2) {
+                //     return false;
+                // }
+
+                // gcd
+                if (gcdArr[j] > returnedPrimes[j]) {
+                    gcdArr[j] = returnedPrimes[j];
+                }
+
+                // lcm
+                if (lcmArr[j] < returnedPrimes[j]) {
+                    lcmArr[j] = returnedPrimes[j];
+                }
+            }
+        }
+
+        // System.out.println("countArr: " + Arrays.toString(countArr));
+        // System.out.println("gcdArr: " + Arrays.toString(gcdArr));
+        // System.out.println("lcmArr: " + Arrays.toString(lcmArr));
+
+        for (int i = 0; i < primes.length; i++) {
+            if (gcdArr[i] != 4) {
+                lcmArr[i] += gcdArr[i];
+            }
+            if (lcmArr[i] != countArr[i]) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
-    private long getLcm(long a, long b) {
-        return (a * b) / getGcd(a, b);
-    }
+    // 8 -> {3, 0, 0, 0}
+    private int[] getPrimes(int n) {
+        int[] countArr = new int[] {0, 0, 0, 0};
 
-    private long getValuesOtTwo(long a, long b) {
-        long ap = a / (GL * LL);
-        a %= (GL * LL);
-        long ag = a / LL;
-        long al = a % LL;
+        int q = n;
+        int i = 0;
+        while (q > 1) {
+            // System.out.println(q + "");
 
-        long bp = b / (GL * LL);
-        b %= (GL * LL);
-        long bg = b / LL;
-        long bl = b % LL;
-
-        long val = ap * bp;
-        val = val * GL + getGcd(ag, bg);
-        val = val * LL + getLcm(al, bl);
-        return val;
+            int p = primes[i];
+            if (q % p == 0) {
+                countArr[i]++;
+                q /= p;
+            } else {
+                i++;
+            }
+        }
+        return countArr;
     }
 }
