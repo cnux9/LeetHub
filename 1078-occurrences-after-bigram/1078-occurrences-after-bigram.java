75d474fc7bbf691201306c7df1c5a4fb692db9bd
cnux9 <65856226+cnux9@users.noreply.github.com>
2025-08-02 23:58:57 +0900

    Time: 1 ms (14.42%), Space: 41.7 MB (40.93%) - LeetHub

---
+++ 1078-occurrences-after-bigram/1078-occurrences-after-bigram.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String[] findOcurrences(String text, String first, String second) {
+        List<String> result = new ArrayList<>();
+        String[] words = text.split(" ");
+        boolean[] firstCheck = new boolean[words.length];
+        boolean[] secondCheck = new boolean[words.length];
+
+        for (int i = 0; i < words.length - 2; i++) {
+            firstCheck[i] = words[i].equals(first);
+        }
+        for (int i = 1; i < words.length - 1; i++) {
+            secondCheck[i] = words[i].equals(second);
+        }
+        for (int i = 2; i < words.length; i++) {
+            if (firstCheck[i - 2] && secondCheck[i - 1]) {
+                result.add(words[i]);
+            }
+        }
+        return result.stream().toArray(String[]::new);
+    }
+}
