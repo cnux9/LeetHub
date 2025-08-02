216cdceb8e75471a21c88ca11349e7301d68aeb0
cnux9 <65856226+cnux9@users.noreply.github.com>
2025-08-03 00:09:30 +0900

    Time: 1 ms (14.42%), Space: 41.5 MB (93.72%) - LeetHub

--- 1078-occurrences-after-bigram/1078-occurrences-after-bigram.java
+++ 1078-occurrences-after-bigram/1078-occurrences-after-bigram.java
@@ -3,10 +3,8 @@ class Solution {
         List<String> result = new ArrayList<>();
         String[] words = text.split(" ");
         for (int i = 0; i < words.length - 2; i++) {
-            if (words[i].equals(first)) {
-                if (words[i + 1].equals(second)) {
-                    result.add(words[i + 2]);
-                }
+            if (words[i].equals(first) && words[i + 1].equals(second)) {
+                result.add(words[i + 2]);
             }
         }
         return result.toArray(String[]::new);
