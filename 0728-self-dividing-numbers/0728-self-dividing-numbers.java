696e981301f157c216f17d1ad9eee7b54ad2c44b
cnux9 <65856226+cnux9@users.noreply.github.com>
2025-08-07 15:35:18 +0900

    Time: 2 ms (76.13%), Space: 42 MB (10.07%) - LeetHub

--- 0728-self-dividing-numbers/0728-self-dividing-numbers.java
+++ 0728-self-dividing-numbers/0728-self-dividing-numbers.java
@@ -10,20 +10,17 @@ class Solution {
     }
 
     private boolean isSelfDividingNumber(int n) {
-        Set<Integer> digits = new HashSet<>();
+        List<Integer> digits = new ArrayList<>();
         int left = n;
         while (left > 0) {
             int digit = left % 10;
             if (digit == 0) {
                 return false;
             }
-            digits.add(digit);
-            left /= 10;
-        }
-        for (int digit : digits) {
             if (n % digit != 0) {
                 return false;
             }
+            left /= 10;
         }
         return true;
     }
