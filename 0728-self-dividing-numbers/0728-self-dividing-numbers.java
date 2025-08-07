308dbd7eb7202a4caa1de0276146f1d288c08dc8
cnux9 <65856226+cnux9@users.noreply.github.com>
2025-08-07 16:20:28 +0900

    Time: 2 ms (76.13%), Space: 41.7 MB (14.15%) - LeetHub

--- 0728-self-dividing-numbers/0728-self-dividing-numbers.java
+++ 0728-self-dividing-numbers/0728-self-dividing-numbers.java
@@ -1,6 +1,6 @@
 class Solution {
     public List<Integer> selfDividingNumbers(int left, int right) {
-        List<Integer> result = new ArrayList<>(right - left);
+        List<Integer> result = new ArrayList<>();
         for (int i = left; i <= right; i++) {
             if (isSelfDividingNumber(i)) {
                 result.add(i);
@@ -10,7 +10,6 @@ class Solution {
     }
 
     private boolean isSelfDividingNumber(int n) {
-        List<Integer> digits = new ArrayList<>();
         int left = n;
         while (left > 0) {
             int digit = left % 10;
