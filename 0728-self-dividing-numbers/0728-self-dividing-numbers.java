3dcb0decc0fd014dcc739690bae0cbef6e2dccc0
cnux9 <65856226+cnux9@users.noreply.github.com>
2025-08-07 16:21:03 +0900

    Time: 209 ms (6.33%), Space: 45.2 MB (5.48%) - LeetHub

--- 0728-self-dividing-numbers/0728-self-dividing-numbers.java
+++ 0728-self-dividing-numbers/0728-self-dividing-numbers.java
@@ -1,26 +1,51 @@
 class Solution {
     public List<Integer> selfDividingNumbers(int left, int right) {
         List<Integer> result = new ArrayList<>();
-        for (int i = left; i <= right; i++) {
-            if (isSelfDividingNumber(i)) {
+        int[] digits = getDigits(right + 1);
+        for (int i = right; i >= left; i--) {
+            // System.out.print(Arrays.toString(digits) + " ");
+            int j;
+            for (j = 0; digits[j] == 0; j++) {
+
+            }
+            digits[j] -= 1;
+            for (j -= 1; j >= 0; j--) {
+                digits[j] = 9;
+            }
+
+
+            if (isSelfDividingNumber(digits, i)) {
                 result.add(i);
             }
         }
+        Collections.reverse(result);
         return result;
     }
 
-    private boolean isSelfDividingNumber(int n) {
-        int left = n;
-        while (left > 0) {
-            int digit = left % 10;
-            if (digit == 0) {
-                return false;
-            }
-            if (n % digit != 0) {
-                return false;
-            }
-            left /= 10;
+    private int[] getDigits(int n) {
+        ArrayList<Integer> digits = new ArrayList<>();
+        while (n > 0) {
+            digits.add(n% 10);
+            n /= 10;
         }
+        return digits.stream().mapToInt(i -> i).toArray();
+    }
+
+    private boolean isSelfDividingNumber(int[] digits, int n) {
+        System.out.print(Arrays.toString(digits) + " ");
+        int i;
+        for (i = digits.length - 1; digits[i] == 0; i--) {
+
+        }
+        System.out.print(n + " ");
+        for (int j = 0; j <= i; j++) {
+            int digit = digits[j];
+            if (digit == 0 || n % digit != 0) {
+                System.out.println(digit);
+                return false;
+            }
+        }
+        System.out.println("true");
         return true;
     }
 }
