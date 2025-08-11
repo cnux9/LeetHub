99f5e3c52a34ab04096afdb41157302e1bd293cb
cnux9 <65856226+cnux9@users.noreply.github.com>
2025-08-11 21:46:26 +0900

    Time: 4 ms (95.74%), Space: 44.8 MB (33.52%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean judgeCircle(String moves) {
+        int x = 0;
+        int y = 0;
+        for (char c : moves.toCharArray()) {
+            if (c == 'U') {
+                y++;
+            } else if (c == 'D') {
+                y--;
+            } else if (c == 'L') {
+                x--;
+            } else {
+                x++;
+            }
+        }
+        return y == 0 && x == 0;
+    }
+}
