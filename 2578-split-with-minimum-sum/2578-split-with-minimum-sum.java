beac0fd7f540a1e0cd6060f4bd677d884526ee25
cnux9 <65856226+cnux9@users.noreply.github.com>
2025-08-18 23:48:44 +0900

    Time: 1 ms (87.93%), Space: 40.5 MB (65.52%) - LeetHub

---
+++ 2578-split-with-minimum-sum/2578-split-with-minimum-sum.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int splitNum(int num) {
+        int[] digits = new int[10];
+        for (int i = 0; num > 0; i++) {
+            digits[i] = num % 10;
+            num /= 10;
+        }
+        Arrays.sort(digits);
+
+        int a = 0;
+        int b = 0;
+        boolean isA = true;
+        for (int digit : digits) {
+            if (isA) {
+                a = a * 10 + digit;
+            } else {
+                b = b * 10 + digit;
+            }
+            isA = !isA;
+        }
+        return a + b;
+    }
+}
