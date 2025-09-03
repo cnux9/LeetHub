fd46ac85bb127b0556bb050a9f3fe8c6e60d6671
cnux9 <65856226+cnux9@users.noreply.github.com>
2025-09-03 22:39:00 +0900

    Time: 24 ms (5.25%), Space: 44.8 MB (5.71%) - LeetHub

---
+++ 2678-number-of-senior-citizens/2678-number-of-senior-citizens.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int countSeniors(String[] details) {
+        int count = 0;
+        for (String detail : details) {
+            int age = Integer.valueOf("" + detail.charAt(11) + detail.charAt(12));
+            System.out.println(age);
+            if (age > 60) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
