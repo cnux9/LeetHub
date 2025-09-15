539074886e6eae8bbcded9d94969c7e2841f7816
cnux9 <65856226+cnux9@users.noreply.github.com>
2025-09-15 12:47:55 +0900

    Time: 0 ms (100%), Space: 44.1 MB (94.92%) - LeetHub

--- 3396-minimum-number-of-operations-to-make-elements-in-array-distinct/3396-minimum-number-of-operations-to-make-elements-in-array-distinct.java
+++ 3396-minimum-number-of-operations-to-make-elements-in-array-distinct/3396-minimum-number-of-operations-to-make-elements-in-array-distinct.java
@@ -2,7 +2,6 @@ class Solution {
     public int minimumOperations(int[] nums) {
         boolean[] bools = new boolean[100];
         for (int i = nums.length - 1; i >= 0; i--) {
-            System.out.println(bools[nums[i] - 1]);
             if (bools[nums[i] - 1] == true) {
                 return (i + 1) / 3 + ((i + 1) % 3 > 0 ? 1 : 0);
             }
