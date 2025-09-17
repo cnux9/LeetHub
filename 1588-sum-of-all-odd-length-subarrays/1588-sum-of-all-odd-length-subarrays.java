d70a1c7067bddade1c96a8f51aa8fa12cc4b0a74
cnux9 <65856226+cnux9@users.noreply.github.com>
2025-09-17 10:53:44 +0900

    Time: 0 ms (100%), Space: 41.2 MB (79.46%) - LeetHub

--- 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.java
+++ 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.java
@@ -1,14 +1,9 @@
 class Solution {
     public int sumOddLengthSubarrays(int[] arr) {
         int sum = 0;
-        int kind = (arr.length + 1) / 2 * 2 - 1;
         for (int i = 0; i < arr.length; i++) {
-            int repeat = 0;
-            for (int l = 1; l <= kind; l+=2) {
-                int val1 = l - (arr.length - i);
-                int val2 = (i < l -1 ? i :  l - 1) - (0 > val1 ? 0 : val1) + 1;
-                repeat += val2 > 0 ? val2 : 0;
-            }
+            int repeat = ((i + 1) * (arr.length - i) + 1) / 2;
+            // System.out.println("    " + (i + 1) * (arr.length - 1));
             // System.out.println("i: " + i + ", arr[i]: " + arr[i] + ", repeat: " + repeat);
             sum += arr[i] * repeat;
         }
