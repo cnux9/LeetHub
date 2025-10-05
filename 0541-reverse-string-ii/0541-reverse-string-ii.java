13b91b65130a4f5796eddadf46b1b89f61ea29ed
cnux9 <65856226+cnux9@users.noreply.github.com>
2025-10-05 19:34:36 +0900

    Time: 6 ms (6.21%), Space: 44.6 MB (7.35%) - LeetHub

--- 0541-reverse-string-ii/0541-reverse-string-ii.java
+++ 0541-reverse-string-ii/0541-reverse-string-ii.java
@@ -2,6 +2,7 @@ class Solution {
     public String reverseStr(String s, int k) {
         StringBuilder sb = new StringBuilder();
 
+        char[] charArray = s.toCharArray();
         for (int i = 0; i < s.length(); i++) {
             // System.out.println(i + " " + s.charAt(i));
             int r = i % (2 * k);
@@ -9,14 +10,14 @@ class Solution {
                 if ((i - r) + k - r - 1 > s.length() - 1) {
                     r = i;
                     for (i = i; i < s.length(); i++) {
-                        sb.append(s.charAt(s.length() + r - i - 1));
+                        sb.append(charArray[s.length() + r - i - 1]);
                     }
                     break;
                 } else {
-                    sb.append(s.charAt((i - r) + k - r - 1));
+                    sb.append(charArray[(i - r) + k - r - 1]);
                 }
             } else {
-                sb.append(s.charAt(i));
+                sb.append(charArray[i]);
             }
         }
         return sb.toString();
